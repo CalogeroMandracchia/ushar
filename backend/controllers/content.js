12adbd8f1f680c62bdef50003ff5e45f201c995b
Mandracchia <cmandracchia@deloitte.it>
2017-06-03 16:22:05 +0200

    grafica aggiunta

--- backend/controllers/content.js
+++ backend/controllers/content.js
@@ -1,13 +1,21 @@
-
+(function (exports, require, module, __filename, __dirname) { 
 const lib = require('../../public/common/libServer');
 
 //download
 const download = (req, res) =>
 {
-  const root = "C:/Users/cmandracchia/Desktop";
-  const path = root + '/' + req.params.file;
-
-  res.download(path,  (err) => { if (err) { console.error(res.headersSent); return; }  console.log("download done!"); });
+    const root = "C:/Users/cmandracchia/Desktop";
+    const path = root + '/' + req.params.file;
+    res.download(path,  (err) => 
+    { 
+        if (err)
+        {
+            console.error(err.message);
+            console.error("header sent: " + res.headersSent); 
+            return; 
+        }  
+        console.log("downloaded! -> " + req.params.file); 
+    });
 }
 
 
@@ -15,3 +23,4 @@ const download = (req, res) =>
 module.exports = {
   download,
 }
+});
